6e400197a4152cb85c70b01171479721f418ab98
Peter Onyegbule <49274497+webstellar@users.noreply.github.com>
2022-07-25 13:16:31 +0100

    Deletion

--- frontend/src/components/user/Register.js
+++ frontend/src/components/user/Register.js
@@ -76,7 +76,7 @@ const Register = () => {
 
   useScript("https://accounts.google.com/gsi/client", () => {
     window.google.accounts.id.initialize({
-      client_id: process.env.local.REACT_APP_GOOGLE_CLIENT_ID,
+      client_id: process.env.REACT_APP_GOOGLE_CLIENT_ID,
       callback: onGoogleSignIn,
     });
 
