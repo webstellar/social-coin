f9d9ed1b1e276aaa10c4d660aa0d6977a8c77f80
Tushar Inani <tusharinani93@gmail.com>
2022-08-02 17:36:19 +0530

    Fix: using findOne to locate the id of a doc

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -15,7 +15,7 @@ exports.isAuthenticatedUser = catchAsyncErrors(async (req, res, next) => {
   }
 
   const decoded = jwt.verify(token, process.env.JWT_SECRET);
-  req.user = await User.findById(decoded.id);
+  req.user = await User.findOne({_id:decoded.id});
 
   next();
 });
