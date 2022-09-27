a56e2b1506552e261a69365075264c08fc393b6a
Peter Onyegbule <49274497+webstellar@users.noreply.github.com>
2022-09-27 10:33:13 +0100

    auth revamp

--- frontend/src/components/config/linkedinConfig.js
+++ frontend/src/components/config/linkedinConfig.js
@@ -1,7 +1,8 @@
 export const LinkedInApi = {
-    clientId: '77si3kcxh3yyzp',
-    redirectUrl: 'http://localhost:3000/register',
-    oauthUrl: 'https://www.linkedin.com/oauth/v2/authorization?response_type=code',
-    scope: 'r_liteprofile%20r_emailaddress',
-    state: '123456'
+  clientId: "77si3kcxh3yyzp",
+  redirectUrl: "https://socialcoinproj.herokuapp.com/register",
+  oauthUrl:
+    "https://www.linkedin.com/oauth/v2/authorization?response_type=code",
+  scope: "r_liteprofile%20r_emailaddress",
+  state: "123456",
 };
