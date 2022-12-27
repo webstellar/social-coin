fefa9cea141f65c8a420714ce4e0ca9dc9d471bc
Peter Onyegbule <49274497+webstellar@users.noreply.github.com>
2022-12-27 04:31:11 +0100

    Updates

--- client/src/config/linkedInconfig.js
+++ client/src/config/linkedInconfig.js
@@ -1,6 +1,6 @@
 export const LinkedInApi = {
   clientId: "77si3kcxh3yyzp",
-  redirectUrl: "https://socialcoinproj.herokuapp.com/register",
+  redirectUrl: "https://hhero.org/register",
   oauthUrl:
     "https://www.linkedin.com/oauth/v2/authorization?response_type=code",
   scope: "r_liteprofile%20r_emailaddress",
